01f3fa57b899d3dbdd65311b133622947871f9ea
suman-1000 <sumangayen1000@gmail.com>
2025-02-04 00:33:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,8 +35,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://movie1:movie1@movie1.ifhuc.mongodb.net/?retryWrites=true&w=majority&appName=movie1")
+DATABASE_NAME = environ.get('DATABASE_NAME', "movie 1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
