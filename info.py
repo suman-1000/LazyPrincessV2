68be87de80dfe832ce859c4264d0c2ed362fe005
suman-1000 <sumangayen1000@gmail.com>
2025-02-04 00:29:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'LazyPrincess')
-API_ID = int(environ.get('API_ID', '13323016'))
-API_HASH = environ.get('API_HASH', '68e791e616100248b0a53ae86a661a12')
+API_ID = int(environ.get('API_ID', '26078268'))
+API_HASH = environ.get('API_HASH', 'fd0442eaf9e376706050dc8cada3ca4d')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 #Port
